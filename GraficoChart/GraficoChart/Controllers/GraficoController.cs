ead36295910fa957731a438cf894257e8c837b36
esantos <esantos@MODERN-NOC02>
2017-02-22 13:30:02 -0300

    Ok

---
+++ GraficoChart/GraficoChart/Controllers/GraficoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GraficoChart.Controllers
+{
+    public class GraficoController : Controller
+    {
+        // GET: Grafico
+        public ActionResult ExemploGrafico()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
